d9e82cbb7e66058505ff6f6dd2ac454c0869a81b
Onwaba <onwaba@projectcodex.co>
2016-01-13 09:47:08 +0200

    added phone view

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ var express = require('express'),
 
 var ObjectId = mongodb.ObjectId;
 
-
 var app = express();
-//var url = 'mongodb://localhost:27017/free_basics';
 
 app.use(bodyParser.urlencoded({extended: false}));
 //parse/application/json
@@ -21,35 +19,38 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
-//app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
- 
-// // Connection URL 
+
+ // Connection URL 
  var url = 'mongodb://localhost:27017/free_basics';
-// // Use connect method to connect to the Server 
-// MongoClient.connect(url, function(err, db) {
-//   assert.equal(null, err);
-//   console.log("Connected correctly to server");
  
-//   db.close();
-// });
+ var paths = {
+    "question1" : "question2",
+    "question2" : "about_you",
+    "about_you" : "financial_info",
+    "financial_info":"puzzles",
+    "puzzles" : ""
+
+ }
+ //var path = req.path;
 
 app.get("/", function (req,res) {
     res.render("home")
 });
 
-app.get('/applicationForm',function(req, res, next){
-    res.render('question1');
+app.get('/applicationForm',function (req,res, next){
+    var route = req.path;
+    console.log(route)
+    res.render('question1' );
 });
 
-app.post('/create',newApplicant.application);
+app.post('/applicationForm',newApplicant.application);
 
-//app.get("/applicationForm", function (req,res) {
-    //res.render("question1")
-//});
 
 
 app.get("/applicationForm/question2/:id", function (req,res) {
+    var route = req.path;
+    console.log("http:/" + route)
     res.render("question2", {_id : req.params.id });
  });
 app.post('/applicationForm/question2/:id', function (req,res) {
@@ -91,6 +92,8 @@ app.get("/codecademy", function (req,res) {
   res.render("codecademy")
 });
 app.get("/applicationForm/financial_info/:id", function (req,res) {
+     var route = req.path;
+    console.log("http:/" + route)
      res.render("financial_info", {_id : req.params.id });
 });
 app.post('/applicationForm/financial_info/:id', function (req,res) {
@@ -127,6 +130,8 @@ app.post('/applicationForm/financial_info/:id', function (req,res) {
     });
 });
 app.get("/applicationForm/about_you/:id", function (req,res) {
+      var route = req.path;
+      console.log("http:/" + route)
      res.render("about_you",{_id: req.params.id});
 });
 app.post('/applicationForm/about_you/:id', function (req,res) {
@@ -157,6 +162,8 @@ app.post('/applicationForm/about_you/:id', function (req,res) {
     });
 });
 app.get("/applicationForm/puzzles/:id", function (req,res) {
+    var route = req.path;
+      console.log("http:/" + route)
      res.render("puzzles",{_id:req.params.id});
 });
 app.post('/applicationForm/puzzles/:id', function (req,res) {
